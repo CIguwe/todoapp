4e9c206852bc115e60b2b4323f71cd9660d34904
Chidera Iguwe <iguwec@gmail.com>
2020-09-04 14:32:05 -0400

    updated server

--- server/app.js
+++ server/app.js
@@ -2,19 +2,24 @@ import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 import helmet from 'helmet';
-import { connect } from 'mongoose';
+import mongoose from 'mongoose';
 
+const CONNECTION_URL =
+  'mongodb+srv://chidera_iguwe:5-Kingdoms@deramongocluster.p7xp8.mongodb.net/todoapp?retryWrites=true&w=majority';
 export const app = express();
 
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
 export const start = async () => {
   try {
-    await connect();
+    await mongoose.connect(CONNECTION_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     app.listen(3000, () => {
       console.log('listening on port 3000');
     });
